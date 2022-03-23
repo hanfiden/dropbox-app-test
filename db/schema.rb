32eaca6d4f629ed8b43afb83781582c95c33f165
Han Fiden <fidendu2@gmail.com>
2022-03-23 16:16:43 +0100

    Add remain_amount column to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_090721) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_23_150516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_090721) do
     t.float "total_amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "remains_amount"
   end
 
   create_table "terms", force: :cascade do |t|
